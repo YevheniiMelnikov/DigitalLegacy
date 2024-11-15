573b7163e9e824b4c964736b48f482a098fd6f34
YevheniiMelnikov <evgeniy@workmail.com>
2024-11-15 12:17:00 +0200

    Main page template added

--- DigitalLegacy/DigitalLegacy/settings.py
+++ DigitalLegacy/DigitalLegacy/settings.py
@@ -93,7 +93,11 @@ TIME_ZONE = "Europe/Kyiv"
 USE_I18N = True
 USE_TZ = True
 
-STATIC_URL = "static/"
+STATIC_URL = '/static/'
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
+
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
 REST_FRAMEWORK = {
--- DigitalLegacy/main/views.py
+++ DigitalLegacy/main/views.py
@@ -1,5 +1,26 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.core.mail import send_mail
+from django.contrib import messages
 
 
 def index(request):
-    return render(request, "main/index.html", {"title": "ЄСпадщина"})
+    return render(request, "main/main.html", {"title": "ЄСпадщина"})
+
+
+def contact(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        email = request.POST.get("email")
+        message = request.POST.get("message")
+
+        send_mail(
+            f"Сообщение от {name}",
+            message,
+            email,
+            ["info@espadshchina.ua"],
+            fail_silently=False,
+        )
+
+        messages.success(request, "Ваше повідомлення було відправлено успішно!")
+        return redirect("index")
+    return render(request, "main/contact.html")
